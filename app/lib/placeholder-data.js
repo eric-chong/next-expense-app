ea13f94b12e05017d81d6e6e31848fc7a6ba3ac9
Eric Chong <hoichieric@hotmail.com>
2024-02-10 15:32:23 -0800

    Add budget and expense placeholder data :rabbit:

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -3,12 +3,179 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
+    name: 'Demo User',
     email: 'user@nextmail.com',
     password: '123456',
   },
 ];
 
+const budgets = [
+  {
+    id: 'f2e4d51b-ec94-4a72-bdc0-7d37c0dd3ea5',
+    start_date: '2024-01-01',
+    end_date: '2024-12-31',
+    user_id: users[0].id,
+  },
+];
+
+const mortgageBudgetItem = {
+  id: 'd3d4c1c9-c58b-4f0c-a653-894c80d8312f',
+  name: 'Mortgage',
+  description: 'Mortgage payment',
+  amount: 3774.43,
+  budgetId: budgets[0].id,
+};
+const carInsuranceBudgetItem = {
+  id: 'a298600f-8f57-4742-b59f-967b2a0b0ecd',
+  name: 'Car Insurance',
+  description: 'Car insurance monthly set aside',
+  amount: 160,
+  budgetId: budgets[0].id,
+};
+const gasBudgetItem = {
+  id: 'c8901b26-bd00-4ac5-aa3c-421ac178a80a',
+  name: 'Gas',
+  description: 'Gas fill up',
+  amount: 440,
+  budgetId: budgets[0].id,
+};
+const mealOutBudgetItem = {
+  id: 'f0d1daac-f598-40c2-82a6-9da2d233e71a',
+  name: 'Meal Out',
+  description: 'Meal out at restaurants',
+  amount: 850,
+  budgetId: budgets[0].id,
+};
+const propertyTaxBudgetItem = {
+  id: '8aa91e25-41d6-4ec2-a4d0-a3e9a431f059',
+  name: 'Property Tax',
+  description: 'Property tax monthly set aside',
+  amount: 360,
+  budgetId: budgets[0].id,
+};
+const bcHydroBudgetItem = {
+  id: '6eed184e-ef3d-4d12-b8b6-6c74a0a471ac',
+  name: 'BC Hydro',
+  description: 'BC Hydro payment',
+  amount: 65,
+  budgetId: budgets[0].id,
+};
+const fortisBudgetItem = {
+  id: 'b59124ce-1468-4416-a80d-92a56d89afbd',
+  name: 'Fortis',
+  description: 'Fortis BC payment',
+  amount: 100,
+  budgetId: budgets[0].id,
+};
+const shawBudgetItem = {
+  id: 'baa40fb1-f597-43de-ac8c-d4a3a16ede83',
+  name: 'Shaw',
+  description: 'Shaw internet payment',
+  amount: 84,
+  budgetId: budgets[0].id,
+};
+const groceryBudgetItem = {
+  id: 'dd5b348f-8c3c-4a7a-b823-e0f94028c82d',
+  name: 'Grocery',
+  description: 'Grocery shopping',
+  amount: 300,
+  budgetId: budgets[0].id,
+};
+const violinBudgetItem = {
+  id: '34d6b7ea-fbd7-45e6-ba50-23735dd6580b',
+  name: 'Violin',
+  description: 'Violin lessons',
+  amount: 500,
+  budgetId: budgets[0].id,
+};
+
+const budgetItems = [
+  mortgageBudgetItem,
+  carInsuranceBudgetItem,
+  gasBudgetItem,
+  mealOutBudgetItem,
+  propertyTaxBudgetItem,
+  bcHydroBudgetItem,
+  fortisBudgetItem,
+  shawBudgetItem,
+  groceryBudgetItem,
+  violinBudgetItem,
+];
+
+const expenseItems = [
+  {
+    id: 'b28b040b-4d87-4d3b-9015-bf1778421315',
+    date: '2024-02-01',
+    amount: 160,
+    description: null,
+    budgetItemId: carInsuranceBudgetItem.id,
+  },
+  {
+    id: 'a9d9de4b-0811-4f11-83fd-6d1a59e6a595',
+    date: '2024-02-01',
+    amount: 360,
+    description: null,
+    budgetItemId: propertyTaxBudgetItem.id,
+  },
+  {
+    id: '2532852b-a074-43a2-8538-f9da738b437a',
+    date: '2024-02-01',
+    amount: 224.67,
+    description: null,
+    budgetItemId: fortisBudgetItem.id,
+  },
+  {
+    id: 'b1c55332-949e-4d77-94c5-27a1bdf9866b',
+    date: '2024-02-01',
+    amount: 57.95,
+    description: 'YY birthday cake',
+    budgetItemId: mealOutBudgetItem.id,
+  },
+  {
+    id: '493a685b-2b1d-42a6-bed5-6555109fcc3f',
+    date: '2024-02-01',
+    amount: 42.42,
+    description: 'Walmart',
+    budgetItemId: groceryBudgetItem.id,
+  },
+  {
+    id: '01389294-7018-4f2a-95fb-eb1b5d5350b8',
+    date: '2024-02-02',
+    amount: 17.76,
+    description: 'Lost in 50s',
+    budgetItemId: mealOutBudgetItem.id,
+  },
+  {
+    id: '1d710aeb-bcd2-423b-87d3-d303503a315c',
+    date: '2024-02-03',
+    amount: 84,
+    description: 'Shaw',
+    budgetItemId: shawBudgetItem.id,
+  },
+  {
+    id: 'd994401d-c8cb-421a-a4a5-77aac3b13f07',
+    date: '2024-02-02',
+    amount: 43,
+    description: 'Shaw',
+    budgetItemId: mealOutBudgetItem.id,
+  },
+  {
+    id: '9f0043b6-3c49-4591-8a20-60ca201dabaa',
+    date: '2024-02-03',
+    amount: 19,
+    description: 'Maxim',
+    budgetItemId: mealOutBudgetItem.id,
+  },
+  {
+    id: '92735887-afec-495b-946c-6feeaca0e689',
+    date: '2024-02-03',
+    amount: 15,
+    description: "McDonald's",
+    budgetItemId: mealOutBudgetItem.id,
+  },
+];
+
+// TODO: Remove these placeholder data when clean up
 const customers = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
@@ -182,6 +349,11 @@ const revenue = [
 
 module.exports = {
   users,
+  budgets,
+  budgetItems,
+  expenseItems,
+
+  // TODO: remove these place holder data when clean up
   customers,
   invoices,
   revenue,
