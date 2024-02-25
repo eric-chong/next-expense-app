aebb57a1e4d58c79db272f68372ca1dd8f8b0f50
Eric Chong <hoichieric@hotmail.com>
2024-02-24 17:48:45 -0800

    Add next rewrites :rabbit:

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/budgets/:budgetId/:path*',
+        destination: '/budgets/:budgetId/',
+      },
+      { source: '/budgets/current/:path*', destination: '/budgets/current/' },
+    ];
+  },
+};
 
 module.exports = nextConfig;
