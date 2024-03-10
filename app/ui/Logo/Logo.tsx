565f0b85a9db90e70238c6b8343eb361968701a0
Eric Chong <hoichieric@hotmail.com>
2024-03-10 14:42:31 -0700

    Change Logo to use mui Box :rabbit:

--- app/ui/Logo/Logo.tsx
+++ app/ui/Logo/Logo.tsx
@@ -1,13 +1,21 @@
 import PaidIcon from '@mui/icons-material/Paid';
 import { lusitana } from '@/app/ui/fonts';
+import { Box, Typography } from '@mui/material';
 
 export default function Logo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    <Box
+      className={`${lusitana.className}`}
+      display="flex"
+      flexDirection="row"
+      lineHeight="1"
+      alignItems="center"
+      color="white"
     >
       <PaidIcon sx={{ fontSize: 40, marginRight: '0.5rem' }} />
-      <p className="text-[34px]">Planner</p>
-    </div>
+      <Typography className={`${lusitana.className}`} variant="h4">
+        Planner
+      </Typography>
+    </Box>
   );
 }
