4ee958bc9f7c71dbaee3e137b0847160e5fb7052
Eric Chong <hoichieric@hotmail.com>
2024-04-06 11:58:46 -0700

    Autocomplete loading :rabbit:

--- app/ui/Table/TableCellField/AutocompleteField.tsx
+++ app/ui/Table/TableCellField/AutocompleteField.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import { Autocomplete, CircularProgress, TextField } from '@mui/material';
 import { IBaseCellField } from './TableCellField';
 
 export default function AutocompleteField({
@@ -9,18 +9,33 @@ export default function AutocompleteField({
   editingRowData,
   value,
 }: IBaseCellField) {
+  const prevEditingRowData = useRef(editingRowData);
+  const [isOpen, setIsOpen] = useState(false);
   const [options, setOptions] = useState([]);
+  const [loadingOptions, setLoadingOptions] = useState(false);
 
   useEffect(() => {
     async function fetchOptions() {
+      setLoadingOptions(true);
       const options = column.selectOptions
         ? await (column.selectOptions as Function)(editingRowData)
         : [];
+      setLoadingOptions(false);
       setOptions(options);
     }
 
-    fetchOptions();
-  }, [column.selectOptions, editingRowData]);
+    if (isOpen && options.length === 0) {
+      fetchOptions();
+    }
+  }, [isOpen, options.length, column.selectOptions, editingRowData]);
+
+  useEffect(() => {
+    const { name } = column;
+    if (name && prevEditingRowData.current[name] === editingRowData[name]) {
+      setOptions([]);
+    }
+    prevEditingRowData.current = editingRowData;
+  }, [editingRowData]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Autocomplete
@@ -29,12 +44,23 @@ export default function AutocompleteField({
       defaultValue={value}
       disableClearable
       options={options}
+      onOpen={() => setIsOpen(true)}
+      onClose={() => setIsOpen(false)}
+      loading={loadingOptions}
       renderInput={(params) => (
         <TextField
           {...params}
           InputProps={{
             ...params.InputProps,
             onChange: (e) => onChange(e.target.value),
+            endAdornment: (
+              <>
+                {loadingOptions ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
             error: hasError,
             size: 'small',
           }}
