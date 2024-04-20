9383bf9f9bed45a55fff03e32a3e65bd142f751e
Eric Chong <hoichieric@hotmail.com>
2024-04-19 23:22:37 -0700

    Add expense charts drawer :rabbit:

--- app/spa/pages/expense/Page.tsx
+++ app/spa/pages/expense/Page.tsx
@@ -4,12 +4,15 @@ import { Box, Typography } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '@/app/spa/App';
 import { BudgetItem, ExpenseItem } from '@/app/types';
+import DrawerProvider from '@/app/spa/providers/DrawerProvider';
 import GlobalAlertProvider from '@/app/spa/providers/GlobalAlertProvider';
+import DrawerContainer from '@/app/ui/DrawerContainer';
+import GlobalAlert from '@/app/ui/GlobalAlert';
 import SectionHeader from '@/app/ui/SectionHeader';
+import ViewChartsButton from '@/app/ui/ViewChartsButton';
 import ExpensesPageSkeleton from '@/app/ui/skeletons/expenses';
 import { ExpenseItemsTable, MonthNavigator } from './components';
 import ExpenseSummaryTable from './components/ExpenseSummaryTable';
-import GlobalAlert from '@/app/ui/GlobalAlert';
 
 interface IPage {
   budgetItems: Array<BudgetItem>;
@@ -42,33 +45,52 @@ function ExpensesPage({ budgetItems, expenseItems }: IPage) {
   return (
     <main>
       <GlobalAlertProvider>
-        <GlobalAlert />
-        <Box display="flex" flexDirection="column" gap="1rem">
-          <Typography variant="h5">Expenses</Typography>
-          <MonthNavigator />
-          <Box
-            display="flex"
-            sx={{
-              flexDirection: { xs: 'column', sm: 'column', md: 'row' },
-              gap: { xs: '2rem', sm: '2rem', md: '1rem' },
-            }}
-          >
-            <Box flexGrow="1">
-              <SectionHeader>Expense items</SectionHeader>
-              <ExpenseItemsTable
-                budgetItems={budgetItems}
-                expenseItems={expenseItems}
-              />
+        <DrawerProvider>
+          <GlobalAlert />
+          <DrawerContainer title="Expense charts">
+            {/* <BudgetCharts budgetItems={budgetItems} summaryData={summaryData} /> */}
+            Test
+          </DrawerContainer>
+          <Box display="flex" flexDirection="column" gap="1rem">
+            <Typography variant="h5">Expenses</Typography>
+            <Box
+              display="flex"
+              gap={{ xs: '0.5rem', sm: '0.5rem', md: '1rem' }}
+              sx={{
+                flexDirection: { xs: 'column', sm: 'column', md: 'row' },
+              }}
+            >
+              <Box flexGrow="1">
+                <MonthNavigator />
+              </Box>
+              <Box padding={{ xs: '0 0.5rem', sm: '0 0.5rem', md: '0' }}>
+                <ViewChartsButton disabled={false} />
+              </Box>
             </Box>
-            <Box flexBasis="40%">
-              <SectionHeader>Expense summary</SectionHeader>
-              <ExpenseSummaryTable
-                budgetItems={budgetItems}
-                expenseItems={expenseItems}
-              />
+            <Box
+              display="flex"
+              sx={{
+                flexDirection: { xs: 'column', sm: 'column', md: 'row' },
+                gap: { xs: '2rem', sm: '2rem', md: '1rem' },
+              }}
+            >
+              <Box flexGrow="1">
+                <SectionHeader>Expense items</SectionHeader>
+                <ExpenseItemsTable
+                  budgetItems={budgetItems}
+                  expenseItems={expenseItems}
+                />
+              </Box>
+              <Box flexBasis="40%">
+                <SectionHeader>Expense summary</SectionHeader>
+                <ExpenseSummaryTable
+                  budgetItems={budgetItems}
+                  expenseItems={expenseItems}
+                />
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </DrawerProvider>
       </GlobalAlertProvider>
     </main>
   );
